75c668988d5b677c60aa7c3ff17ec5d9c2aa6741
prashtrisula <ajinata89@gmail.com>
2022-04-19 09:28:14 +0700

    another fix to routing

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ export default function App() {
             <MainSection />
             <AnimatePresence exitBeforeEnter>
               <Routes location={location} key={location.pathname}>
-                <Route path="" element={<Home />} />
+                <Route path="/" element={<Home />} />
                 <Route path="/projects" element={<Projects />} />
                 <Route path="/contact" element={<Contact />} />
               </Routes>
--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
+    <HashRouter basename={`/${process.env.PUBLIC_URL}/`}>
       <ChakraProvider>
         <App />
       </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </>
 );
 
